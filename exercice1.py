a67fd516d0521596ec2e32ba3cb929cc34c0447e
Azunyth <alexis.vernot@gmail.com>
2017-02-18 17:33:38 +0100

    Exercice 1 correction

---
+++ exercice1.py
@@ -0,0 +1,20 @@
+a = 5
+b = 3
+
+print("a : {}, b : {}".format(a, b))
+
+print("1ere technique")
+
+a = a + b
+b = a - b
+a = a - b
+
+print("a : {}, b : {}".format(a, b))
+
+print("2e technique")
+
+c = a
+a = b
+b = c
+
+print("a : {}, b : {}".format(a, b))
