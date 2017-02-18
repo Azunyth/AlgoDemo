0cd6bb60abd6b3a2d1117b270eb76bf94c935c59
Azunyth <alexis.vernot@gmail.com>
2017-02-18 17:51:43 +0100

    Correction exercice 3

---
+++ exercice3.py
@@ -0,0 +1,7 @@
+a = int(input("Entrer le numerateur :"))
+b = int(input("Entrer le denominateur :"))
+
+if a > 0 and b != 0:
+    print("Resultat de la divison : ", a/b)
+else:
+    print("Oops, erreur de saisie !")
