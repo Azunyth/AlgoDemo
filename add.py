975cd2cf4d3448c48cdcff6c6c120f0f962e82cf
Azunyth <alexis.vernot@gmail.com>
2017-02-25 17:19:25 +0100

    Correction exercice de la fonction d'addition

---
+++ add.py
@@ -0,0 +1,11 @@
+def sumNumbers(n1, n2, n3) :
+    """
+	Le mot clef return permet de renvoyer une valeur
+	ici la valeur est l'addition des 3 paramètres
+    """
+    return n1 + n2 + n3
+
+
+total = sumNumbers(5,3,8)
+
+print(total)
